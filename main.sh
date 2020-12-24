1c804e6f132a984779e3e91b2159c1c2ff777ba2
Kry9toN <dhimasbagusprayoga@gmail.com>
2020-12-24 10:12:12 +0700

    add expect to rsa known host

--- main.sh
+++ main.sh
@@ -81,7 +81,7 @@ if [ $RELEASE = true ]; then
     function sendRelese() {
             PATH="/root/tools/clang/bin:${PATH}"
             curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-                            -d chat_id=$chat_id \
+                            -d chat_id=$chat_id_rilis \
                             -d "disable_web_page_preview=true" \
                             -d "parse_mode=html" \
                             -d text="<b>🔥KryPtoN Kernel</b> is <b>Release</b>%0A📱 Device: $DEVICE%0ALinux version: $LINUX_VERSION%0A🆑 Changelog : <code>$CHANGELOG</code>%0A<a href='https://kryptonproject.my.id/'>⬇️ Download</a>"
@@ -90,7 +90,7 @@ if [ $RELEASE = true ]; then
     #Upload
     function upload() {
         cd /root/AnyKernel
-        sshpass -p ${SF_PASS} kry9ton@frs.sourceforge.net:/home/frs/project/krypton-project > /dev/null 2>&1 <<EOF
+        spawn sshpass -p ${SF_PASS} kry9ton@frs.sourceforge.net:/home/frs/project/krypton-project > /dev/null 2>&1 <<EOF
 mkdir $DEVICE
 cd $DEVICE
 mkdir -p $SF_PATH
@@ -98,6 +98,12 @@ cd $SF_PATH
 put $ZIP_NAME
 exit
 EOF
+        expect {
+            "RSA key fingerprint" {
+             send "yes\r"
+            }
+        }
+        expect "#"
     }
 fi
 
