606ee510e026437eda414947390ea745ba4519f1
Dhimas Bagus Prayoga <dhimasbagusprayoga@gmail.com>
2021-02-17 02:51:30 +0700

    Update release.sh

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
     function upload() {
         cd /root/AnyKernel
         ssh-keyscan -H frs.sourceforge.net >> ~/.ssh/known_hosts
-        sshpass -p $SF_PASS sftp -oBatchMode=no kry9ton@frs.sourceforge.net:/home/frs/project/krypton-project > /dev/null 2>&1 <<EOF
+        sshpass -p "$SF_PASS" sftp -oBatchMode=no kry9ton@frs.sourceforge.net:/home/frs/project/krypton-project > /dev/null 2>&1 <<EOF
 mkdir $DEVICE
 cd $DEVICE
 mkdir -p $SF_PATH
@@ -15,10 +15,10 @@ EOF
     # Send info plox channel
     function sendRelese() {
             curl -F photo=@img/icon.jpg "https://api.telegram.org/bot$token/sendphoto" \
-                            -d chat_id=$chat_id \
-                            -d "disable_web_page_preview=true" \
-                            -d "parse_mode=html" \
-                            -d caption="<b>🔥KryPtoN Kernel</b> is <b>Release</b>%0A%0A📱 Device: $DEVICE%0A%0A🆑 Changelog : <code>$(echo $CHANGELOG | sed 's/<br>/%0A/g')</code>%0A%0A💸 Donate Me if you like my work%0A<a href='https://www.paypal.me/KomodoOS'>Paypal</a> | <a href='https://saweria.co/donate/Kry9toN'>Saweria</a> for Indonesian%0A%0A<a href='http://t.me/KKgrupofficial'>👥 Group</a> | <a href='http://t.me/KryPtoNKernel'>📺 Channel</a>%0A%0A<a href='https://kryptonproject.my.id/'>⬇️ Download</a>"
+                            -F chat_id=$chat_id \
+                            -F "disable_web_page_preview=true" \
+                            -F "parse_mode=html" \
+                            -F caption="<b>🔥KryPtoN Kernel</b> is <b>Release</b>%0A%0A📱 Device: $DEVICE%0A%0A🆑 Changelog : <code>$(echo $CHANGELOG | sed 's/<br>/%0A/g')</code>%0A%0A💸 Donate Me if you like my work%0A<a href='https://www.paypal.me/KomodoOS'>Paypal</a> | <a href='https://saweria.co/donate/Kry9toN'>Saweria</a> for Indonesian%0A%0A<a href='http://t.me/KKgrupofficial'>👥 Group</a> | <a href='http://t.me/KryPtoNKernel'>📺 Channel</a>%0A%0A<a href='https://kryptonproject.my.id/'>⬇️ Download</a>"
     }
 
 function gitpush() {
