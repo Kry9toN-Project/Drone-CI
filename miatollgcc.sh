c97c7ba9f521e990291e49c7eb6df9ef58ef547c
itsmeley <itsmeley@nndrive.my.id>
2021-05-15 17:47:33 +0700

    update

--- miatollgcc.sh
+++ miatollgcc.sh
@@ -2,8 +2,8 @@
 ZIP_NAME="${CODENAME}-BLC-MIATOLL-AOSP-${tanggal}.zip"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 START=$(date +"%s")
-CROSS_COMPILE="/root/tools/11/bin/aarch64-elf"
-CROSS_COMPILE_ARM32="/root/tools/arm11/bin/arm-eabi"
+CROSS_COMPILE="/root/tools/11/bin/aarch64-elf-"
+CROSS_COMPILE_ARM32="/root/tools/arm11/bin/arm-eabi-"
 export CROSS_COMPILE
 export CROSS_COMPILE_ARM32
 export ARCH=arm64
