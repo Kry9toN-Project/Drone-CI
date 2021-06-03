b0cdb478ce05bff92a54f3f57ff2fa2bcf3e0d15
itsmeley <itsmeley@nndrive.my.id>
2021-06-04 00:53:32 +0700

    update

--- miui.sh
+++ miui.sh
@@ -2,7 +2,10 @@
 ZIP_NAME="${CODENAME}-BLC-MIATOLL-MIUI-${tanggal}.zip"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 START=$(date +"%s")
-export PATH="/root/tools/clang/bin:${PATH}"
+GCC="/root/tools/gcc-eva-arm64/bin/aarch64-elf-"
+GCC32="/root/tools/gcc-eva-arm/bin/arm-eabi-"
+export GCC
+export GCC32
 export ARCH=arm64
 export KBUILD_BUILD_USER=Zoel
 export KBUILD_BUILD_HOST=Thanksyou
@@ -14,12 +17,7 @@ CATEGORIE="MIUI"
 # Compile plox
 function compile() {
         make O=out ARCH=arm64 cust_defconfig
-        make -j$(nproc --all) O=out \
-                      ARCH=arm64 \
-		      LOCALVERSION="-${CODENAME}-${tanggal}" \
-                      CC=clang \
-                      CROSS_COMPILE=aarch64-linux-gnu- \
-		      CROSS_COMPILE_ARM32=arm-linux-gnueabi- 2>&1 | tee build.log
+	make -s -C $(pwd) CROSS_COMPILE=${GCC} CROSS_COMPILE_ARM32=${GCC32} O=out -j32 2>&1 | tee build.log
             if ! [ -a $IMAGE ]; then
                 finerr
 		stikerr
