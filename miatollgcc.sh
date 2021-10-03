d470d037389747d13e24e82943646f293db9a27d
weedrr <botnancy1@gmail.com>
2021-10-04 02:50:53 +0700

    update

--- miatollgcc.sh
+++ miatollgcc.sh
@@ -6,10 +6,9 @@ GCC="/root/tools/gcc-eva-arm64/bin/aarch64-elf-"
 GCC32="/root/tools/gcc-eva-arm/bin/arm-eabi-"
 export GCC
 export GCC32
-export LD=ld.lld
 export ARCH=arm64
-export KBUILD_BUILD_USER=Zoel
-export KBUILD_BUILD_HOST=Thanksyou
+export KBUILD_BUILD_USER=dindascape
+export KBUILD_BUILD_HOST=notabook
 SF_PATH="AOSP"
 
 # path link web
